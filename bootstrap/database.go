94ca9f9e9fea47930871bc32ad9bb97de133c061
ijull <119686454+IJ4L@users.noreply.github.com>
2023-12-01 22:09:43 +0800

    fix: enc

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -14,7 +14,7 @@ type Client struct {
 }
 
 func NewMySQLDatabase(env *Env) *Client {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", env.User, env.Password, env.Host, env.Port, env.Database)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/clean?charset=utf8mb4&parseTime=True&loc=Local", env.User, env.Password, env.Host, env.Port, env.Database)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
